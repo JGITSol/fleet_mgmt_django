c6ce0205123f6b8d9eea3b1203ebbb7c66bad378
JGITSol <behemotpl@gmail.com>
2025-09-26 02:09:00 +0200

    migrations(accounts): fix 0007 to AddField for license_expiry_date (DB already had column)

--- CarFleetManagement/accounts/migrations/0007_alter_driver_license_expiry.py
+++ CarFleetManagement/accounts/migrations/0007_alter_driver_license_expiry.py
@@ -7,7 +7,9 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
+        # The field did not exist in the original create migration (0005_driver).
+        # Use AddField to create the column with the nullable/blank properties.
+        migrations.AddField(
             model_name="driver",
             name="license_expiry_date",
             field=models.DateField(null=True, blank=True),
